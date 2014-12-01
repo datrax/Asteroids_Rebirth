df6f8c4f93f99bc02cd07cd91189ed1ce4aed5be
datrax <datrax@mail.ru>
2014-12-01 17:53:37 +0200

    first steps

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Asteroids_Rebirth
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+
+    interface IDrawing
+    {
+        Canvas canvas { get; set; }
+        void draw();
+    }
+    public partial class MainWindow : Window
+    {
+        Spaceship spaceship;
+        bool FoolScreen = false;
+        public MainWindow()
+        {
+
+            InitializeComponent();
+
+
+        }
+
+        private void Keys(object sender, KeyEventArgs e)
+        {
+
+            if (Keyboard.IsKeyDown(Key.W)){
+                spaceship.moveUp();
+
+            }
+            if (Keyboard.IsKeyDown(Key.S))
+            {
+                spaceship.moveDown();
+            }
+            if (Keyboard.IsKeyDown(Key.A))
+            {
+                spaceship.angle -= 3;
+
+            }
+            if (Keyboard.IsKeyDown(Key.D))
+            {
+                spaceship.angle += 3;
+
+            }
+            spaceship.draw();
+
+            //Activates foolscreen mod
+            if (Keyboard.IsKeyDown(Key.Enter) && Keyboard.Modifiers == ModifierKeys.Alt)
+            {
+                if (FoolScreen == false)
+                {
+                    this.WindowStyle = WindowStyle.None;
+                    this.WindowState = WindowState.Maximized;
+
+                }
+                else
+                {
+                    this.WindowStyle = WindowStyle.SingleBorderWindow;
+                    this.WindowState = WindowState.Normal;
+                }
+                FoolScreen = !FoolScreen;
+            }
+          
+        }
+
+        private void Scailing(object sender, double ScaleWidth, double ScaleHeight)
+        {
+
+                Canvas c = sender as Canvas;
+                ScaleTransform st = new ScaleTransform();
+                c.RenderTransform = st;
+                    st.ScaleX *= ScaleWidth;
+                    st.ScaleY *= ScaleHeight;
+                    return;
+
+        }
+
+        private void test(object sender, MouseButtonEventArgs e)
+        {
+            //MessageBox.Show(canvas.ActualWidth + " " + myViewBox.ActualWidth + " " + this.Width + " " + this.ActualHeight);
+            MessageBox.Show(Math.Sin(Math.PI * (-34) / 180.0).ToString());
+        }
+
+        private void sizechangerefresh(object sender, SizeChangedEventArgs e)
+        {
+            //Scailing(canvas, this.ActualWidth / 497.0, this.Height / 320.0);
+        }
+
+        private void construct(object sender, RoutedEventArgs e)
+        {
+            myViewBox.StretchDirection = StretchDirection.Both;
+            myViewBox.Stretch = Stretch.Fill;
+            spaceship = new Spaceship(canvas, 20, 10);
+
+        }
+    }
+}
---
+++ Spaceship.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Asteroids_Rebirth
+{
+    class Spaceship:IDrawing
+    {
+        public double x{get;set;}
+        public double y{get;set;}
+        public double speedX { get; set; }
+        public double speedY { get; set; }
+        public double angle { get; set; }
+        public Canvas canvas { get; set; }
+        Image image;
+
+        void LoadPicture()
+        {
+           image = new Image
+            {
+                Width = 10,
+                Height = 10,
+                Name = "BodyName",
+                Source = new BitmapImage(new Uri(Environment.CurrentDirectory + @"\F5S4.png")),
+            };
+        }
+        public Spaceship(Canvas canvas,double x,double y)
+        {
+            this.canvas = canvas;
+            this.x = x;
+            this.y = y;
+            angle = 0;
+            LoadPicture();
+            canvas.Children.Add(image);
+            draw();
+        }
+        public void draw()
+        {
+            Canvas.SetTop(image, y);
+            Canvas.SetLeft(image, x);
+            RotateTransform rotateTransform1 =
+             new RotateTransform(angle);
+            rotateTransform1.CenterX = 3;
+            rotateTransform1.CenterY = 5;
+            image.RenderTransform = rotateTransform1;
+        }
+        public void moveUp()
+        {
+            x += Math.Sin(Math.PI * angle / 180.0);
+            y -= Math.Cos(Math.PI * angle / 180.0);
+        }
+        public void moveDown()
+        {
+            x -= Math.Sin(Math.PI * angle / 180.0);
+            y += Math.Cos(Math.PI * angle / 180.0);
+        }
+
+    }
+}
